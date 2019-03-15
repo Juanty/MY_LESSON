1128a96df3dfe08bfe59781c4ce31fcf975350c1
Huang <40397349+Juanty@users.noreply.github.com>
2019-03-16 01:27:18 +0800

    Update random.js

--- algorithm/hongbao/random.js
+++ algorithm/hongbao/random.js
@@ -1,19 +1,19 @@
-// let ran_num = Math.random（）;
+// let ran_num = Math.random();
 
-//（）=> {}函数表达式
-const  hongbao  =（total，num）=> {
-    const  arr  = []; //待分配的金额
+//()=>{}函数表达式
+const hongbao = (total,num)=>{
+    const arr = [];//待分配的金额
     //定义计算量
-    让 restAmount = total，restNum = num;
-    for（let i =  0 ; i < num - 1 ; i ++）{
-        //前n-1个个人发随机金额
-        //（restAmount / restNum）* 2每人的随机金额最大值为平均值的两倍
-        让量=  parseFloat（数学。随机（）*（（restAmount / restNum）* 2 - 0））。toFixed（2）;
-        restAmount -  = amount;
-        restNum - ;
-        arr。推（金额）;
+    let restAmount = total, restNum = num;
+    for(let i = 0;i<num-1;i++){
+        //前n-1个人发随机金额
+        //(restAmount/restNum)*2 每人的随机金额最大值为平均值的两倍
+        let amount = parseFloat(Math.random()*((restAmount/restNum)*2-0)).toFixed(2);
+        restAmount -=amount;
+        restNum--;
+        arr.push(amount);
     }
-    arr。推（restAmount。toFixed（2））;
-    返回 ARR;
+    arr.push(restAmount.toFixed(2));
+    return arr;
 }
-控制台。日志（红宝（200，10））;
+console.log(hongbao(200,10));
