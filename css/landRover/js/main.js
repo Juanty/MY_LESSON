7a00b0b1b3097b3d15cc3625c361e2c07be970db
Jaunty <541975984@qq.com>
2019-04-22 22:54:07 +0800

    landRover

--- css/landRover/js/main.js
+++ css/landRover/js/main.js
@@ -19,9 +19,23 @@
   // carousel
   $('.ui.hero.carousel')
     .slick(
-    {
-      prevArrow: '<button class="ui prev bottom button"><i class="material-icons">chevron_left</i></button>',
-      nextArrow: '<button class="ui next bottom button"><i class="material-icons">chevron_right</i></button>'
-    }
+      {
+        prevArrow: '<button class="ui prev bottom button"><i class="material-icons">chevron_left</i></button>',
+        nextArrow: '<button class="ui next bottom button"><i class="material-icons">chevron_right</i></button>'
+      }
     )
+    // semantic.js
+    $('.ui.sidebar')
+    .sidebar('setting', 'dimPage', false)
+    .sidebar('attach events', 
+            '.ui.navbar .menu-icon', 
+            'show')
+    .sidebar('attach events',
+            '.ui.sidebar .close-icon',
+            'hidden')
+    // sidebar
+    $('.ui.navbar .menu')
+    .clone()
+    .appendTo('.ui.sidebar')
+    // $('.ui.sidebar).append();
 })();
