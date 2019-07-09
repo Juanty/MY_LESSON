77c15a063b754f146fbb2c52dfe573b483f36d7b
DeMo_J <541975984@qq.com>
2019-07-09 17:05:37 +0800

    react-ant-router-demo

---
+++ React/react-ant-router-demo/src/page/Label2.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+class Label extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        Label2
+      </div>
+     );
+  }
+}
+ 
+export default Label;
