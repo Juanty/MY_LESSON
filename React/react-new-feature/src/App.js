48ec631645c2d32c99bfc0779b3658877083a557
DeMo_J <541975984@qq.com>
2019-07-25 14:38:35 +0800

    componentWillMount...运行机制

--- React/react-new-feature/src/App.js
+++ React/react-new-feature/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ConcurrnetModeDemo from './concurrent/index'
-
+import Index from './lifeCycle-react15/index'
+//hooks function 组件增强
 function App() {
+  const [ parentCount, setParentCount ] = useState(0);
   return (
     <div>
+      <button onClick={() => {
+        setParentCount (parentCount + 1)
+      }}>
+        setParentCount
+      </button>
+      <Index parentCount={parentCount}/>
       <ConcurrnetModeDemo />
     </div>
   );
