e15dd934c398f1682112e1119cba4fa858035fdb
DeMo_J <541975984@qq.com>
2019-07-25 11:10:59 +0800

    concurrent并发

---
+++ React/react-new-feature/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ConcurrnetModeDemo from './concurrent/index'
+
+function App() {
+  return (
+    <div>
+      <ConcurrnetModeDemo />
+    </div>
+  );
+}
+
+export default App;
