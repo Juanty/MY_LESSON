0b4bfa7dadfdaf5835c3a068d9478430064dc8a2
DeMo_J <541975984@qq.com>
2019-07-29 21:11:25 +0800

    trip

--- trip-server/routes/index.js
+++ trip-server/routes/index.js
@@ -1,25 +1,23 @@
-var express = require('express');
-var fs = require("fs");
-var router = express.Router();
+var express = require('express')
+var fs = require('fs')
+var router = express.Router()
 
 // 首页的操作
 router.get('/', function(req, res, next) {
-// res.statusCode = 200;
-// res.setHeader('Content-Type', 'application/json');
-function readImage(path,res){
-  console.log(path)
-  fs.readFile(path,'binary',function(err,  file)  {
+  // res.statusCode = 200
+  // res.setHeader('Content-Type', 'application/json')
+  function readImage(path, res) {
+    fs.readFile(path, 'binary', function(err, file) {
       if (err) {
-          console.log(err);
-          return;
-      } else{
-          console.log("输出文件");
-          res.writeHead(200,  {'Content-Type':'image/jpeg'});
-          res.write(file,'binary');
-          res.end();
+        return
+      } else {
+        res.writeHead(200, {'Content-Type': 'image/jpeg'})
+        res.write(file, 'binary')
+        res.end()
       }
-  });
-}
-readImage('/public/images/head.jpeg', res)
-});
-module.exports = router;
+    })
+  }
+  readImage('/public/images/head.jpeg', res)
+})
+
+module.exports = router
